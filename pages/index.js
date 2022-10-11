47daddb3120fbe49285447f3108e4430b558c337
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-10-11 16:13:15 +0300

    for vercel

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Home from './components/screens/home/Home';
+import Home from '../app/components/screens/home/Home';
 import axios from 'axios';
 import { API_URL } from '../app/constants';
 
@@ -6,14 +6,28 @@ export default function HomePage(props) {
     return <Home {...props} />;
 }
 export const getStaticProps = async () => {
-    const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
-    const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
-
-    return {
-        props: {
-            links,
-            me,
-        },
-        revalidate: 60,
-    };
+    try {
+        const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
+        const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
+        if (!links || !me) {
+            return {
+                notFound: true,
+            };
+        }
+        return {
+            props: {
+                links,
+                me,
+            },
+            revalidate: 60,
+        };
+    } catch {
+        return {
+            props: {
+                links: null,
+                me: null,
+            },
+            revalidate: 60,
+        };
+    }
 };
