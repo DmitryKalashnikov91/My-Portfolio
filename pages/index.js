090883ec31a6fd51aa134056cbeccdce2c42bcdd
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-10-13 09:36:17 +0300

    for vercel

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,13 @@ export default function HomePage(props) {
     return <Home {...props} />;
 }
 export const getServerSideProps = async () => {
-    try {
-        const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
-        const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
-        if (!links || !me) {
-            return {
-                notFound: true,
-            };
-        }
-        return {
-            props: {
-                links,
-                me,
-            },
-        };
-    } catch {
-        return {
-            props: {
-                links: null,
-                me: null,
-            },
-        };
-    }
+    const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
+    const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
+
+    return {
+        props: {
+            links,
+            me,
+        },
+    };
 };
