3c2ae10a3369b80abaad47f2799c9ad5078d4eab
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-10-17 11:37:39 +0300

    add try catch

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,24 @@ import { API_URL } from '../app/constants';
 export default function HomePage(props) {
     return <Home {...props} />;
 }
-export const getServerSideProps = async () => {
-    const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
-    const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
-
-    return {
-        props: {
-            links,
-            me,
-        },
-    };
+export const getStaticProps = async () => {
+    try {
+        const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
+        const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
+        if (!links && !me) {
+            return {
+                notFound: true,
+            };
+        }
+        return {
+            props: {
+                links,
+                me,
+            },
+        };
+    } catch {
+        return {
+            props: null,
+        };
+    }
 };
