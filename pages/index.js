0918022606ca4fe1fcafabe67d8ea2bf740a3ace
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-10-18 12:51:09 +0300

    vercel

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,10 @@ export const getStaticProps = async () => {
         };
     } catch {
         return {
-            props: null,
+            props: {
+                links,
+                me,
+            },
         };
     }
 };
