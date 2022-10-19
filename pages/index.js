5457f323ee38d840e2f0e02f3d5c46fa91a81c3e
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-10-19 17:25:07 +0300

    vercel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import Home from '../app/components/home/Home';
-import { API_URL } from '../app/constants';
 import links from '../app/api/links.json';
 import me from '../app/api/me.json';
 
@@ -14,28 +12,4 @@ export const getStaticProps = () => {
             me,
         },
     };
-    // try {
-    //     const resLinks = await axios.get(`${API_URL}/links`);
-    //     const links = resLinks.data;
-    //     const resMe = await axios.get(`${API_URL}/me`);
-    //     const me = resMe.data;
-    //     if (!links) {
-    //         return {
-    //             notFound: true,
-    //         };
-    //     }
-    //     return {
-    //         props: {
-    //             links,
-    //             me,
-    //         },
-    //     };
-    // } catch {
-    //     return {
-    //         props: {
-    //             links: null,
-    //             me: null,
-    //         },
-    //     };
-    // }
 };
